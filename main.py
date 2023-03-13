5ecb043cbc75e589125d17795cd0ebe13c5a487b
David Calle <davidcalleg@gmail.com>
2023-03-12 21:54:17 -0500

    Further visualization

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from constructivo import constructivo
 from store import storeData
 import matplotlib.pyplot as plt
 
-file = "mtVRP1.txt"
+file = "test.txt"
 nodesNumber, vehicles, capacity, autonomy, deposit, nodes = getData(file)
 
 routes, distances, time = constructivo(nodes, vehicles, autonomy, capacity, True)
 storeData(routes, distances, time)
 
-fig, axs = plt.subplots(1, 4,  figsize=(9, 9), sharey=True)
+fig, axs = plt.subplots(1, vehicles,  figsize=(9, 9), sharey=True)
 
 index = 0
 for route in routes:
+    xAxis = [node[1] for node in nodes]
+    yAxis = [node[2] for node in nodes]
+    axs[index].scatter(xAxis, yAxis)
+
     xAxis = [nodes[node][1] for node in route]
     yAxis = [nodes[node][2] for node in route]
     axs[index].plot(xAxis, yAxis, label=str(index))
