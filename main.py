6be5dacc0cd7bb4abe7e9e0397669a6e8ec9f2bb
David Calle <davidcalleg@gmail.com>
2023-03-12 21:40:56 -0500

    graphic help

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from read import *
 from constructivo import constructivo
 from store import storeData
+import matplotlib.pyplot as plt
 
-file = "test.txt"
+file = "mtVRP1.txt"
 nodesNumber, vehicles, capacity, autonomy, deposit, nodes = getData(file)
 
-route, distances, time = constructivo(nodes, vehicles, autonomy, capacity)
-storeData(route, distances, time)
+routes, distances, time = constructivo(nodes, vehicles, autonomy, capacity, True)
+storeData(routes, distances, time)
+
+fig, axs = plt.subplots(1, 4,  figsize=(9, 9), sharey=True)
+
+index = 0
+for route in routes:
+    xAxis = [nodes[node][1] for node in route]
+    yAxis = [nodes[node][2] for node in route]
+    print(xAxis)
+    print(yAxis)
+    axs[index].plot(xAxis, yAxis, label=str(index))
+    index += 1
+
+plt.show()
+
+
 
