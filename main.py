9287376f5c4dcf8b7202adbacba12348d0e43b01
David Calle <d.calle@globant.com>
2023-05-31 23:09:58 -0500

    Final Version

--- main.py
+++ main.py
@@ -23,7 +23,10 @@ def twoOpt(route, a, b):
   return route[:a+1] + route[a+1: b+1][::-1] + route[b+1:]
 
 def crossTwoOpt (routeFrom, routeTo, a, b):
-  return [routeFrom[:a+1] + routeTo[b+1:], routeTo[:b+1] + routeFrom[a+1:]   ]
+  return [routeFrom[:a+1] + routeTo[b+1:], routeTo[:b+1] + routeFrom[a+1:]]
+
+def crossTwoOptReversed (routeFrom, routeTo, a, b):
+  return [routeFrom[:a+1] + routeTo[:b+1][::-1] , routeTo[b+1:][::-1] + routeFrom[a+1:]   ]
 
 def threeOpt(route, a, b, c):
   r1 = route[0:a+1] + route[a+1: b+1] + route[b+1 : c+1][::-1] + route[c+1:]
@@ -106,6 +109,28 @@ def distanceOrInvalid(routes):
       return None
   return round(distance, 2)
 
+def singleDistance(route):
+  currentCapacity = capacity
+  distance = 0
+  visited = []
+  for i in range(len(route)):
+    visited.append(route[i])
+    currentCapacity -= demands[route[i]]
+    if(i <= len(route) - 2):
+      distance += distanceMatrix[route[i]][route[i+1]]
+    if route[i] == 0:
+      currentCapacity = capacity
+  return round(distance, 2)
+
+
+def getRouteDistace(routes):
+  distance = 0
+  for route in routes:
+    routeDistance = singleDistance(route)
+    if routeDistance:
+      distance += routeDistance
+  return round(distance, 2)
+
 def getFactivilityScore(route):
   distance = singleDistanceOrInvalid(route)
   if distance == None:
@@ -137,7 +162,6 @@ def feasiblenessNetOrInvalid(routes):
     else:
       return None
   return round(feasibleScore, 2)
-
 
 def twoOptExecuter(routes, bestImprovement=True, T=None, distanceOrInvalid = distanceOrInvalid):
   bestSolution = routes
@@ -190,8 +214,35 @@ def crossTwoOptExecuter(routes, bestImprovement=True, T=None, distanceOrInvalid 
             bestDistance = newDistance
   return(enhanced and  bestSolution)
 
+def crossTwoOptReversedExecuter(routes, bestImprovement=True, T=None, distanceOrInvalid = distanceOrInvalid):
+  bestSolution = routes
+  bestDistance = distanceOrInvalid(routes)
+  enhanced = None
+  for i in range(len(routes)-1):
+    for j in range(i+1, len(routes)):
+      for nodeFrom in range(len(routes[i])-2):
+        for nodeTo in range(len(routes[j])-2):
+          newSolution = routes.copy()
+          newRouteI, newRouteJ = crossTwoOptReversed(newSolution[i], newSolution[j], nodeFrom, nodeTo)
+          newSolution[i] = newRouteI
+          newSolution[j] = newRouteJ
+          newDistance = distanceOrInvalid(newSolution)
+          if(newDistance != None and newDistance < bestDistance):
+            if(not bestImprovement):
+              return newSolution    
+            enhanced = True
+            bestSolution = newSolution
+            bestDistance = newDistance
+          elif T and newDistance != None and random.random() <= e**((bestDistance - newDistance) / T):
+            if(not bestImprovement):
+              return newSolution  
+            enhanced = True
+            bestSolution = newSolution
+            bestDistance = newDistance
+  return(enhanced and  bestSolution)
 
-def randomCrossTwoOptExecuter(routes, T=None, Aleatory=False, limit = 1000):
+
+def randomCrossTwoOptExecuter(routes, T=None, Aleatory=False, limit = 100000):
   GeneratedSolution = None
   bestDistance = distanceOrInvalid(routes)
   iter = 0
@@ -336,11 +387,12 @@ def crossInsertExecuter(routes, bestImprovement=True, T=None, distanceOrInvalid 
               bestDistance = newDistance 
   return(enhanced and  bestSolution)
 
-def randomCrossInsertExecuter(routes, T=None, limit = 1000):
+def randomCrossInsertExecuter(routes, T=None, limit = 100000):
   GeneratedSolution = None
   bestDistance = distanceOrInvalid(routes)
   iter = 0
   while True:
+    iter+= 1
     if(iter >= limit):
       return routes
     i = random.randint(1, len(routes) - 1)
@@ -354,7 +406,9 @@ def randomCrossInsertExecuter(routes, T=None, limit = 1000):
     newSolution[i] = newRouteI
     newSolution[j] = newRouteJ
     newDistance = distanceOrInvalid(newSolution)
-    iter+= 1
+    for route in  newSolution:
+      if len(route) < 3:
+        newDistance = None
     if(newDistance != None):
       return newSolution
 
@@ -385,7 +439,6 @@ def threeOptExecuter(routes, bestImprovement=True, T=None, distanceOrInvalid = d
               bestSolution = newSolution
               bestDistance = newDistance
   return(enhanced and  bestSolution) 
-
 def subRouteSplitter(route):
   if(len(route) <= 1):
     return []
@@ -437,11 +490,11 @@ def subrouteSwapperExecuter(routes, bestImprovement=True, T=None, distanceOrInva
           elif T and newDistance != None and random.random() <= e**((bestDistance - newDistance) / T):
             if(not bestImprovement):
               return newSolution  
-            print("enhanced")
             enhanced = True
             bestSolution = newSolution
             bestDistance = newDistance 
   return(enhanced and  bestSolution)
+
 
 def VNS(routes, algorithmExecuters, bestImprovement = True, T = None, niter = None, distanceOrInvalid=distanceOrInvalid, limitTime = None):
   bestSolution = routes
@@ -461,6 +514,92 @@ def VNS(routes, algorithmExecuters, bestImprovement = True, T = None, niter = No
     if(limitTime != None and time.time() - start > limitTime):
       return bestSolution
   return bestSolution
+
+def RVNS(routes, algorithmExecuters, bestImprovement = True, T = None, distanceMethods=[distanceOrInvalid], limitTime = None, kiter = 2):
+  bestSolution = routes
+  iter = 0
+  algorithm = None
+  while len(algorithmExecuters) > 0:
+    method = random.choice(distanceMethods)
+    newAlgorithm = random.randint(0,  len(algorithmExecuters) -1)
+    if algorithm != None:
+      while(newAlgorithm == algorithm and  len(algorithmExecuters)  > 1):
+        newAlgorithm = random.randint(0,  len(algorithmExecuters) -1)
+    algorithm = newAlgorithm
+
+    newSolution = algorithmExecuters[algorithm](bestSolution, bestImprovement, T, distanceOrInvalid=method)
+    if(newSolution != None ):
+      newDistance = distanceOrInvalid(newSolution)
+      if newDistance != None and  newDistance  < distanceOrInvalid(bestSolution):
+        bestSolution = newSolution
+    else:
+      iter += 1
+    if(limitTime != None and time.time() - start > limitTime):
+      return bestSolution
+    if iter == kiter:
+      return bestSolution
+  return bestSolution
+
+def RVNS2(routes, algorithmExecuters, bestImprovement = True, T = None, distanceMethods=[distanceOrInvalid], limitTime = None):
+  bestSolution = routes
+  algorithm = None
+  while len(algorithmExecuters) > 0:
+    method = random.choice(distanceMethods)
+    newAlgorithm = random.randint(0,  len(algorithmExecuters) -1)
+    if algorithm != None:
+      while(newAlgorithm == algorithm and  len(algorithmExecuters)  > 1):
+        newAlgorithm = random.randint(0,  len(algorithmExecuters) -1)
+    algorithm = newAlgorithm
+
+    newSolution = algorithmExecuters[algorithm](bestSolution, bestImprovement, T, distanceOrInvalid=method)
+
+    if(newSolution != None and distanceOrInvalid(newSolution) < distanceOrInvalid(bestSolution)):
+      bestSolution = newSolution
+    else:
+      algorithmExecuters.pop(algorithm)
+    if(limitTime != None and time.time() - start > limitTime):
+      return bestSolution
+  return bestSolution
+
+def Perturb(solution):
+  solution = copy.deepcopy(solution)
+  for i in range(5):
+    solution = randomCrossTwoOptExecuter(solution)
+    solution = randomCrossInsertExecuter(solution)
+  return solution
+
+def hybridRNVS(routes, algorithmExecuters, niter = 1, bestImprovement = True, T = None,  distanceMethods=[distanceOrInvalid], limitTime = None):
+   bestSolution = routes
+   baseSolution = copy.deepcopy(bestSolution)
+   bestDistance = distanceOrInvalid(bestSolution)
+   for i in range(niter):
+      newSolution = RVNS(baseSolution, algorithmExecuters, kiter=2, distanceMethods=distanceMethods)
+      if newSolution != None:
+        newDistance = distanceOrInvalid(newSolution)
+        if newDistance != None:
+          if newDistance < bestDistance:
+            bestSolution = newSolution
+            bestDistance = newDistance
+      baseSolution = Perturb(newSolution)
+   return bestSolution
+
+def hybridRNVS2(routes, algorithmExecuters, niter = 1, bestImprovement = True, T = None, distanceOrInvalid=distanceOrInvalid, limitTime = None):
+   bestSolution = routes
+   baseSolution = copy.deepcopy(bestSolution)
+   bestDistance = distanceOrInvalid(bestSolution)
+   for i in range(niter):
+      newSolution = RVNS2(baseSolution, algorithmExecuters)
+      if newSolution != None:
+        newDistance = distanceOrInvalid(newSolution)
+        if newDistance != None:
+          if newDistance < bestDistance:
+            bestSolution = newSolution
+            bestDistance = newDistance
+      baseSolution = Perturb(newSolution)
+   return bestSolution
+      
+
+
 
 def VNSMSRSP(Ti, Tf, L, r, nsol, limitTime = None):
   index = 0
@@ -534,6 +673,142 @@ def storeData(routes, distances, time, autonomy, name, instance):
     sheet.write(len(routes), 2, feasable)
     wb.save("mtVRP_DavidCalleGonzalez_" + name + ".xls")
 
+from numpy import nbytes
+def generalize(solution):
+  return [node for node in functools.reduce(lambda x, y: x+y, solution) if node != 0]
+
+def rebuild(route):
+  currentCapacity = capacity
+  solution = []
+  newRoute = [0]
+  currentVehicle = 0
+  for i in range(vehicles):
+    solution.append([])
+  for i in range(len(route)):
+    if currentCapacity < demands[route[i]]:
+      solution[currentVehicle] += newRoute
+      newRoute = [0]
+      
+      currentCapacity = capacity
+      currentVehicle += 1
+    newRoute.append(route[i])
+    currentCapacity -= demands[route[i]]
+
+    if(currentVehicle >= vehicles):
+      currentVehicle = 0
+    if i == len(route) -1:
+      solution[currentVehicle] += newRoute
+  for vehicleRoute in solution:
+    vehicleRoute.append(0)
+  return solution
+
+def normalize(solution):
+  return rebuild(generalize(solution))
+
+def nDistanceOrInvalid(solution):
+  return distanceOrInvalid(normalize(solution))
+
+def nfeasiblenessOrInvalid(solution):
+  return feasiblenessOrInvalid(normalize(solution))
+
+def twoOptMutation(solution):
+  indexFrom = random.randint(1, len(solution)-4)
+  indexTo = random.randint(indexFrom+3, len(solution)-1)
+  return twoOpt(solution, indexFrom, indexTo)
+
+def insertMutation(solution):
+  indexFrom = random.randint(1, len(solution)-1)
+  indexTo = random.randint(1, len(solution)-1)
+  while indexTo == indexFrom:
+    indexTo = random.randint(1, len(solution)-1)
+  return insert(solution, indexFrom, indexTo)
+
+def balancedDistance(solution):
+  if solution == None:
+    return None
+  objF = distanceOrInvalid(solution)
+  feas = feasiblenessOrInvalid(solution)
+  if(objF != None and feas != None):
+    return objF + feas
+  else:
+    return None
+def swapMutation(solution):
+  pass
+
+def swapMutation(solution):
+  pass
+
+def crossGenes(solutionA, solutionB):
+  nodesToChange = random.randint(2,5)
+  index = random.randint(0, len(solutionA) - nodesToChange)
+  extractedNodes = solutionA[index: index + nodesToChange]
+  child = solutionB.copy()
+  for node in extractedNodes:
+    child.remove(node)
+  random.shuffle(extractedNodes)
+  child += extractedNodes
+  return child
+
+def crossInsertGenes(solutionA, solutionB):
+  nodesToChange = random.randint(2,5)
+  index = random.randint(0, len(solutionA) - nodesToChange)
+  indexTo = random.randint(0, len(solutionA) - 1)
+  extractedNodes = solutionA[index: index + nodesToChange]
+  child = solutionB.copy()
+  for node in extractedNodes:
+    child.remove(node)
+  random.shuffle(extractedNodes)
+  for node in extractedNodes[::-1]:
+    child.insert(indexTo, node)
+  return child
+
+
+def mutate(solution):
+  solution = twoOptMutation(solution)
+  solution = insertMutation(solution)
+  return solution
+
+def geneticAlgorithm(nsolx16, niter, nc, mutationRate, selectionRegulator = 1):
+  algorithms = [crossSwapExecuter, insertExecuter, twoOptExecuter, swapExecuter, crossInsertExecuter]
+  population = []
+  bestSolution = []
+  bestDistance = None
+  for i in range(nsolx16):
+    graspSolution = grasp(nodes, vehicles, autonomy, capacity, 0.08, 1)[0]
+    population.append(generalize(graspSolution))
+    
+  start = time.time()
+  for i in range(niter):
+    children = []
+    for j in range(nc):
+      solutionA = random.choice(population)
+      solutionB = random.choice(population)
+      newChild = crossInsertGenes(solutionA, solutionB)
+      if random.random() < 0.1:
+        newChild = mutate(newChild)
+      children.append(newChild)
+    population += children
+    for solution in population:
+      rebuiltSolution = rebuild(solution)
+      solutionDistance = distanceOrInvalid(rebuiltSolution)
+      if bestDistance == None or solutionDistance < bestDistance:
+        bestSolution = rebuiltSolution
+        bestDistance = solutionDistance
+      else:
+        percentualDiference = (solutionDistance / bestDistance - 1) * selectionRegulator
+        if random.random() < percentualDiference:
+          population.remove(solution)
+  return bestSolution
+
+
+def definitiveAlgorithm(kinitial, geneticIterations, geneticChildren, mutationRate, randomSearchAlgorithms, deepSearchAlgorithms, randomIterations, limitTime = None):
+  bestGenetic = geneticAlgorithm(kinitial, geneticIterations, geneticChildren, mutationRate)
+  geneticR = hybridRNVS(bestGenetic, randomSearchAlgorithms, niter = randomIterations)
+  geneticFinal = VNS(geneticR, deepSearchAlgorithms, limitTime=limitTime)
+  #geneticFinalOpt = hybridRNVS(geneticFinal, randomSearchAlgorithms, niter = randomIterations , distanceMethods=[feasiblenessNetOrInvalid, balancedDistance])
+  return geneticFinal
+
+
 
 alpha = 0.05
 nsol = 30
@@ -558,12 +833,19 @@ tf = 0.01
 L = 20
 r = 0.7
 
-results = []
-algorithms = [crossTwoOptExecuter, crossSwapExecuter, threeOptExecuter, insertExecuter, twoOptExecuter, swapExecuter, crossInsertExecuter, subrouteSwapperExecuter]
+algorithms = [subrouteSwapperExecuter, crossTwoOptExecuter, crossSwapExecuter, threeOptExecuter, insertExecuter, twoOptExecuter, swapExecuter, crossInsertExecuter, crossTwoOptReversedExecuter]
 timeLimits =  [300,300,450,600,450,300,450,450,450,450,600,300]
+
+harderAlgorithms = [crossTwoOptExecuter, threeOptExecuter, twoOptExecuter , crossTwoOptReversedExecuter]
+simplerAlgorithms = [crossSwapExecuter, crossInsertExecuter, crossTwoOptExecuter]
+algorithms = [crossTwoOptExecuter, crossSwapExecuter, threeOptExecuter, insertExecuter, twoOptExecuter, swapExecuter, crossInsertExecuter, crossTwoOptReversedExecuter, subrouteSwapperExecuter]
+
+results = []
+resultsTime = []
 
 for i in range(1, 13):
     results.append([])
+    resultsTime.append([])
     instance = "mtVRP" + str(i)
     file =  instance + ".txt"
     print(file)
@@ -571,36 +853,35 @@ for i in range(1, 13):
     distanceMatrix = getDistanceMatrix(nodes)
     demands = getDemands(nodes)
     routes, distances, constTime = constructivoOptimizado(nodes, vehicles, autonomy, capacity)
-    initialPheromone = Q/sum(distances)
-    iterAntRoutes, iterAntDistances, iterAntTime = iterativeAntColony(nodes, vehicles, autonomy, capacity, m2, Q2, a2, b2, c, p2, initialPheromone, niter)
-    antRoutes, antDistances, antTime = antColony(nodes, vehicles, autonomy, capacity, m, Q, a, b, c, p, initialPheromone)
-    noiseRoutes, noiseDistances, noiseTime = noise(nodes, vehicles, autonomy, capacity, r, stdDeviation, nsol)
-    graspRoutes, graspDistances, graspTime = grasp(nodes, vehicles, autonomy, capacity, alpha, nsol)
-
+    #initialPheromone = Q/sum(distances)
+    #iterAntRoutes, iterAntDistances, iterAntTime = iterativeAntColony(nodes, vehicles, autonomy, capacity, m2, Q2, a2, b2, c, p2, initialPheromone, niter)
+    #antRoutes, antDistances, antTime = antColony(nodes, vehicles, autonomy, capacity, m, Q, a, b, c, p, initialPheromone)
+    #noiseRoutes, noiseDistances, noiseTime = noise(nodes, vehicles, autonomy, capacity, r, stdDeviation, nsol)
+    #graspRoutes, graspDistances, graspTime = grasp(nodes, vehicles, autonomy, capacity, alpha, nsol)
+    """
     VNS_start = time.time()
-    VNS_solution = VNS(iterAntRoutes, algorithms, limitTime = timeLimits[i-1])
-    VNS_time = time.time()
+    definitive = VNS(iterAntRoutes, algorithms, limitTime=timeLimits[i-1])
+    VNS_time = time.time() - VNS_start
+    """
 
-    VNSOpt_start = time.time()
-    VNSOpt_solution = VNS(iterAntRoutes, algorithms, limitTime = timeLimits[i-1], distanceOrInvalid= feasiblenessOrInvalid)
-    VNSOpt_time = time.time()
+    definite_start = time.time()
+    definitive = definitiveAlgorithm(200, 30, 650, 0.1, simplerAlgorithms, algorithms, 7, timeLimits[i-1])
+    definite_time = time.time() - definite_start
+    """
+    results[i-1].append(distanceOrInvalid(iterAntRoutes))
+    results[i-1].append(distanceOrInvalid(vns_result))
+    results[i-1].append(distanceOrInvalid(definitive))
+    resultsTime[i-1].append(iterAntTime)
+    resultsTime[i-1].append(VNS_time)
+    resultsTime[i-1].append(definite_time)
+    """
+    print(definite_time)
+    print(distanceOrInvalid(definitive))
 
-    VNSMSRSP_start = time.time()
-    VNSMSRSP_solution =  VNSMSRSP(Ti, Tf, L, r,LS_nsol, limitTime = timeLimits[i-1])
-    VNSMSRSP_time = time.time() - VNSMSRSP_start
-
-    results[i-1].append(antRoutes)
-    results[i-1].append(routes)
-    results[i-1].append(graspRoutes)
-    results[i-1].append(noiseRoutes)
-    results[i-1].append(iterAntRoutes)
-    results[i-1].append(VNS_solution)
-    results[i-1].append(iterAntRoutes)
-    
-
-    storeData(VNS_solution, distanceOrInvalid(VNS_solution), abs(VNS_time), autonomy, "VNS", instance)
-    storeData(VNSOpt_solution, distanceOrInvalid(VNSOpt_solution), abs(VNSOpt_time), autonomy, "VNSOpt", instance)
-    storeData(VNSMSRSP_solution, distanceOrInvalid(VNSMSRSP_solution), abs(VNSMSRSP_time), autonomy, "VNSMSRSP", instance)
+    storeData(definitive, distanceOrInvalid(definitive), abs(definite_time), autonomy, "HybridAlgorithm", instance)
+    #storeData(definitive, distanceOrInvalid(definitive), abs(VNS_time), autonomy, "VNS", instance)
+    #storeData(VNSOpt_solution, distanceOrInvalid(VNSOpt_solution), abs(VNSOpt_time), autonomy, "VNSOpt", instance)
+    #storeData(VNSMSRSP_solution, distanceOrInvalid(VNSMSRSP_solution), abs(VNSMSRSP_time), autonomy, "VNSMSRSP", instance)
     #storeData(noiseVns, distanceOrInvalid(noiseVns) or noiseDistances, abs(endNoise), autonomy, "NOISE", instance)
     #storeData(graspVns, distanceOrInvalid(graspVns) or graspDistances, abs(endGrasp), autonomy, "GRASP", instance)
     #storeData(constructiveVns, distanceOrInvalid(constructiveVns) or distances, abs(endConst), autonomy, "Constructivo", instance)
